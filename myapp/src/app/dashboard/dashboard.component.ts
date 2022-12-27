7ce039520e3124a820669ab289b12d60a8e00edf
Babongui mabika venuza <vhenuzya@yahoo.fr>
2022-12-27 18:24:16 +0100

    guard and logout

--- myapp/src/app/dashboard/dashboard.component.ts
+++ myapp/src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit,Output } from '@angular/core';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -9,9 +10,17 @@ import { Component, OnInit } from '@angular/core';
 export class DashboardComponent implements OnInit {
   displayItems!:string;
 
-  constructor() { }
+  @Output() logoutEmitter: EventEmitter<boolean> = new EventEmitter<boolean>();
+
+  constructor(private router:Router) { }
 
   ngOnInit(): void {
   }
 
+  public logOut(): void {
+    sessionStorage.removeItem('token');
+    sessionStorage.removeItem('refreshToken');
+    localStorage.clear();
+    this.router.navigate(['login']);
+  }
 }
--- myapp/src/app/model/user.ts
+++ myapp/src/app/model/user.ts
@@ -1,7 +1,6 @@
 export class User {
   constructor(public id: number,
     public username: string,
-    public password:string,
     public email: string,
     public firstName: string, public lastName: string,
     public gender: string,
